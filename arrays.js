ed3df28552c318ca1d82154a6f7da44959531f97
shyam-pujari <pujarishyamprasad5@gmail.com>
2024-02-06 21:14:09 +0530

    while, do while class con

--- arrays.js
+++ arrays.js
@@ -91,7 +91,7 @@ console.log(value);
 const key_value = Object.assign({}, obj, obj1);
 console.log(key_value);
 
-let userinput;
+// let userinput;
 const given = () => {
   // userinput = prompt("enter a value")
   if (userinput == "5") {
@@ -100,3 +100,58 @@ const given = () => {
     console.log("nice try");
   }
 };
+attempts = 5;
+// for (let i = attempts; i < attempts.length; i--) {
+//   console.log(`remaining ${i}`);
+// }
+while (attempts !== 0) {
+  console.log(`remaining ${attempts}`);
+  attempts--;
+}
+
+let userinput;
+attempts = 5;
+console.log(`you have ${attempts} remaining attempts`);
+while (attempts !== 0) {
+  // userinput = prompt("enter 'exit' to end loop");
+  if (userinput == "exit") {
+    console.log("loop ended");
+    break;
+  } else if (attempts == 1) {
+    console.log("you have no remaining attempts");
+    break;
+  } else {
+    console.log(`try again, you have ${attempts - 1} remaining attempts`);
+  }
+  attempts--;
+}
+
+// let userinput;
+// attempts = 5;
+
+// while(attempts!== 0){
+//     console.log(`you have ${attempts} remaining attempts`)
+//     userinput = prompt("enter 'exit' to end loop")
+//     console.log(userinput)
+//     if(userinput=="exit"){
+//         console.log("loop ended")
+//         break
+//     }else if (attempts == 1){
+//         console.log("you have no remaining attempts")
+//         break
+//     }//else{console.log(`try again, you have ${attempts-1} remaining attempts`)}
+//     attempts--
+// }
+
+class person {
+  constructor(Name, age) {
+    this.Name = Name;
+    this.age = age;
+  }
+  greet() {
+    console.log(`this is my name ${this.Name} and my age is ${this.age}`);
+  }
+}
+const person1 = new person("shyam", 21);
+person1.greet();
+console.log(person1.Name);
